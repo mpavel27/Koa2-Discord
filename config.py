a6a82bf9c800ed7d01d7bbe8330afbdbffb55145
Pavel Marin <kraiermarin11@gmail.com>
2022-11-20 23:54:40 +0000

    Update config.py

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ class Config():
         self.mainColor = 0xdb3d3d
         self.embedCopyright = "2022 © www.koa2.ro"
         self.guildId = 926314891957121084
-        self.languages = [1044037808949567518, 1044037737021452379]
+        self.languages = [':flag_gb:', ':flag_ro:']
